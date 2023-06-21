bf28c497d2620d6912eb3dd506fb6918f26d67cd
Aakash Yadav <aakashsyadav1999@gmail.com>
2023-06-21 18:42:15 +0530

    ConfigurationManager

--- src/cnnClassifier/constants/__init__.py
+++ src/cnnClassifier/constants/__init__.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 
-CONFIG_FILE_PATH=Path('conifg/config.yaml')
-PARAM_FILE_PATH=Path("param.yaml")
+CONFIG_FILE_PATH=Path("config/config.yaml")
+PARAM_FILE_PATH=Path("params.yaml")
