43d6587c0a7dfbb2d50a2730ac2515607267c8c5
Aakash Yadav <aakashsyadav1999@gmail.com>
2023-08-05 15:10:51 +0530

    Prepare_CallBacks

--- src/cnnClassifier/components/prepare_callbacks.py
+++ src/cnnClassifier/components/prepare_callbacks.py
@@ -25,7 +25,7 @@ class PrepareCallback:
     @property
     def _create_ckpt_callbacks(self):
         return tf.keras.callbacks.ModelCheckpoint(
-            filepath=self.config.checkpoint_model_filepath,
+            filepath=('D:\\vscode\\chiken dataset\\Chiken-Diseases-Classification\\artifacts'),
             save_best_only=True
         )
 
